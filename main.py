7ddb19dfd15a72218904e65ab5a0cb3c0c532b3e
HarveySpecter-coder <greenfreedom7@yandex.ru>
2021-07-19 23:55:48 +0300

    SomethingChanged

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ class DrawerList(ThemableBehavior, MDList):
     def set_color_item(self, instance_item):
         """Called when tap on a menu item."""
 
-        # Set the color of the icon and text for the menu item.
+        # Set the color of the icon and text for the menu item. Bla-bla-bla
         for item in self.children:
             if item.text_color == self.theme_cls.primary_color:
                 item.text_color = self.theme_cls.text_color
